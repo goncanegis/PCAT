6a25ef273b74564478461ae6385ecdf3f04544df
Gonca Negis <gonca.negis@gmail.com>
2022-07-31 17:56:45 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('Hello World');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp', 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
