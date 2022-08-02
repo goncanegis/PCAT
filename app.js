bb16b19ec2e08804579150b19be7c8244f800e2d
Gonca Negis <gonca.negis@gmail.com>
2022-08-02 10:16:33 +0300

    added edit, delete functions to photos

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ejs = require('ejs');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -16,10 +18,16 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // Routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', { photos });
 });
 app.get('/about', (req, res) => {
@@ -29,12 +37,43 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = path.join(__dirname, 'public/uploads');
+
+  if (fs.existsSync(uploadDir) === false) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = path.join(__dirname, 'public/uploads/', uploadedImage.name);
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
 });
 app.get('/photos/:id', async (req, res) => {
   const photo = await Photo.findById(req.params.id);
   res.render('photo', { photo });
+});
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', { photo });
+});
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  await photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+});
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findByIdAndDelete(req.params.id);
+  let deletedImage = path.join(__dirname, 'public', photo.image);
+  fs.unlinkSync(deletedImage);
+  res.redirect('/');
 });
 
 const PORT = 3000;
